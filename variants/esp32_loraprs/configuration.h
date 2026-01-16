db0441f7062374322feb0ddd1285007cd92a3b55
Marco Kubon <dl1mx@gmx.de>
2026-01-16 20:59:46 +0100

    added new hardware ESP32_LORAPRS from sh123
    https://github.com/sh123/esp32_loraprs

---
+++ variants/esp32_loraprs/configuration.h
@@ -0,0 +1,89 @@
+/*
+definitions for esp32_loraprs Board
+*/
+
+#pragma once
+
+#include <Arduino.h>
+
+#include <configuration_global.h>
+
+// esp32_loraprs specific config
+#define MODUL_HARDWARE ESP32_LORAPRS
+#define RF_FREQUENCY 433.175000 // 432.900000   // Hz
+#define LORA_APRS_FREQUENCY 433.775000 // 432.900000   // Hz
+#define SX126X  // some functions differ from SX127x and SX126x in RadioLib based on Semtech Chip
+#define ENABLE_GPS
+#define ENABLE_BMX280
+#define ENABLE_BMP390
+#define ENABLE_AHT20
+#define ENABLE_SHT21
+#define ENABLE_BMX680
+#define ENABLE_MCP23017
+#define ENABLE_INA226
+#define ENABLE_MC811
+#define ENABLE_RTC
+#define ENABLE_SOFTSER
+#define SX126X
+#define TX_POWER_MAX 20   // max 20 dBm
+#define TX_POWER_MIN 2
+#define LORA_PREAMBLE_LENGTH DEFAULT_PREAMPLE_LENGTH  // Same for Tx and Rx
+
+#define BOARD_LED 2    // LED_BUILTIN
+
+#define WAIT_TX 5         // ticks waiting after Lora TX in doTX()
+
+#define TX_OUTPUT_POWER 20
+#define CURRENT_LIMIT 140 // in mA +20dBm are about 120mA -> check if enough headroom 
+
+#define ANALOG_PIN 36
+#define ANALOG_REFRESH_INTERVAL 30 // sec messure intervall
+
+/**
+ * RadioLib Coding Rate: Allowed values range from 5 to 8.
+ * case 5: CR_4_5;
+    case 6: CR_4_6;
+    case 7: CR_4_7;
+    case 8: CR_4_8;
+*/
+#define LORA_CR 6
+
+// RadioLib LoRa Bandwidth Setting in kHz
+#define LORA_BANDWIDTH 250
+
+/** RadioLib Spreading Factor
+ * case 6: SF_6;
+    case 7: SF_7;
+    case 8: SF_8;
+    case 9: SF_9;
+    case 10: SF_10;
+    case 11: SF_11;
+    case 12: SF_12;
+*/
+#define LORA_SF 11
+
+// E22-400M30S (SX1268) + AZ Delivery ESP32 DevKitC v4
+
+#define LORA_RST  26
+#define LORA_DIO0 12
+#define LORA_DIO1 0
+//#define LORA_DIO2 not used
+#define LORA_CS 5
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+#define I2C_SDA SDA_PIN
+#define I2C_SCL SCL_PIN
+
+#define BUTTON_PIN 34
+
+#define OneWire_GPIO 25
+
+#define SX1268_CS LORA_CS
+#define SX1268_IRQ LORA_DIO1
+#define SX1268_RST LORA_RST
+#define SX1268_GPIO LORA_DIO0
+
+#define GPS_RX_PIN 16
+#define GPS_TX_PIN 17
